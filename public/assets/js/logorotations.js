0b646485bdcd3c009265c906682a5ee1cb404dbe
wts-jaydeep <jaydeep.a@wingstechsolutions.com>
2022-08-09 19:37:37 +0530

    logo rotation js file cange

--- public/assets/js/logorotations.js
+++ public/assets/js/logorotations.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(window).on("scroll", function() {
       console.log($(this).scrollTop())
+      console.log(window.location.hostname)
       if($(this).scrollTop() >= 30){
         // set to new image
         $(".itIsAnImage img").attr("src","assets/img/logo2.png");
