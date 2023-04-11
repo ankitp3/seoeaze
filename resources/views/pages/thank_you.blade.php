48eaf39ed6034b0063804926bd736e6ce31fe47d
Rahul M <rahulmourya17@gmail.com>
2023-04-11 13:36:43 +0530

    thankyou page redesign

--- resources/views/pages/thank_you.blade.php
+++ resources/views/pages/thank_you.blade.php
@@ -5,7 +5,9 @@
 @section('header')
 
 @show
-
+<style>
+a {color:#000 !important;}
+</style>
 @section('content')
 @php
 	use  PulkitJalan\GeoIP\GeoIP;
@@ -13,106 +15,113 @@
 	$geoip->setIp(\Request::ip());
 	$country_code = $geoip->getCountryCode();
 @endphp
-	<main class="header-video-sec contactheaderbgimg" style="height: auto!important">
-		<!-- Vedio Content Section Start  -->
-			  <div class="container-flude">
-			  
-			  		<div class="container">
-			  			<div class="row indexheader-video-sec-inner-sec">
-				  			<div class="col-lg-12 col-md-12 col-sm-12 indexheader-video-sec-inner-leftsec">
-				  			</div>
-					</div>
-			  	</div>
-				
-			
-			</div>
-			<!-- Vedio Content Section End -->
-			</main>
+	
+			<section class="testimonial local_seosec md-30">
+						<div class="container">
+							<div class="row">
+								<div class="col-lg-6 d-none d-lg-block">
+									<ol class="carousel-indicators tabs">
+										<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
+											<figure>
+												<img src="assets/img/seo_packages_img/testimonials-01-179x179.webp" class="img-fluid" alt="seoeaze testimonial 1">
+											</figure>
+										</li>
+										<li data-target="#carouselExampleIndicators" data-slide-to="1">
+											<figure>
+												<img src="assets/img/seo_packages_img/testimonials-02-306x306.webp" class="img-fluid" alt="seoeaze testimonial 2">
+											</figure>
+										</li>
+										<li data-target="#carouselExampleIndicators" data-slide-to="2">
+											<figure>
+												<img src="assets/img/seo_packages_img/testimonials-03-179x179.webp" class="img-fluid" alt="seoeaze testimonial 3">
+											</figure>
+										</li>
+									</ol>
+								</div>
+								<div class="col-lg-6 d-flex justify-content-center align-items-center i-can-has-a-colored-thing">
+									<div id="carouselExampleIndicators" data-interval="false" class="carousel slide" data-ride="carousel">
+										<h3>Your Request Has Been Received!</h3>
+										<h1><span class="highlight">Thank You.</span></h1>
+										<h4>You are one step closer to join our valued customers who believe in us.</h4>
+										<div class="carousel-inner">
+											<div class="carousel-item active">
+												<div class="quote-wrapper">
+													<p>Need further assistance? Feel free to reach us at:</p>
+												@if ($country_code == "IN")
+												<div class="col-lg-12 col-md-7 col-sm-12 mt-5">
+													<div class="row">
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec phoneicon">
+															<h3>Phone :</h3>
+															<p><a href="tel:+918375011200">+91-8375011200</a><br>														
+															<a href="tel:+911204166710">+91-120-4166710</a></p>
+														</div>
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec skyoeicon">
+															<h3>SKYPE :</h3>
+															<p>seoeaze</P>
+														</div>
+													</div>
+													<div class="row">
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec enquiryicon">
+															<h3>Enquiry :</h3>
+															<p>info@seoeaze.com</P>
+														</div>
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec websiteicon">
+															<h3>Website :</h3>
+															<p>www.seoeaze.com</P>
+														</div>
+													</div>
+													<div class="row">
+														<div class="col-lg-12 col-md-12 col-sm-12 contactinfo-innersec addressicon">
+															<h3>Address :</h3>
+															<p>10685-B Hazelhurst Dr. #19976, Houston, TX 77043<br>
+															B-710, Plot No-A40, The Ithum, Sector-62, Noida (NCR)</p>
+														</div>
+													</div>
+												</div>
+												@else
+												<div class="col-lg-12 col-md-7 col-sm-12 mt-5">
+													<div class="row">
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec phoneicon">
+															<h3>Phone :</h3>
+															<p><a href="tel:+18447363293">844-SEOEAZE (7363293)</a></P>
+														</div>
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec skyoeicon">
+															<h3>SKYPE :</h3>
+															<p>seoeaze</P>
+														</div>
+													</div>
+													<div class="row">
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec enquiryicon">
+															<h3>Enquiry :</h3>
+															<p><a href="mailto:info@seoeaze.com">info@seoeaze.com</a></p>
+														</div>
+														<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec websiteicon">
+															<h3>Website :</h3>
+															<p>www.seoeaze.com</P>
+														</div>
+													</div>
+													<div class="row">
+														<div class="col-lg-12 col-md-12 col-sm-12 contactinfo-innersec addressicon">
+															<h3>Address :</h3>
+															<p>10685-B Hazelhurst Dr. #19976, Houston, TX 77043</P>
+															
+														</div>
+													</div>
+												</div>
+												@endif											
+					
+												</div>
+											</div>
+											
+										</div>
+										
+									</div>
+								</div>
+							</div>
+						</div>
+				</section>
 
-		<!-- Start GET IN TOUCH sec -->
-		<section class="ourbestcapabilitysec">
-	<div class="container-fluid contactinfo-sec">
-		<div class="container">
-			
-			<h1 class="mb-5 text-center"> Thank You </h1>
-			<h4 class="mt-5 mb-5 text-center">Your Request Has Been Received!</h4>
-				<br>
-			<div class="row">
-				<div class="col-lg-5 col-md-5 col-sm-12 mt-5">
-					<div class="getintuch-sec mr-5">
-						<figure>
-							<img src="assets/img/contact/contactinfo_img.png" alt="">
-						</figure>
-					</div>
-				</div>
-				@if ($country_code == "IN")
-				<div class="col-lg-7 col-md-7 col-sm-12 mt-5">
-					<div class="row">
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec phoneicon">
-							<h3>Phone :</h3>
-							<p>844-SEOEAZE (7363293)</p>
-							<p>+91-8375011200<p>								
-							<p> +91-120-4166710</p>
-						</div>
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec skyoeicon">
-							<h3>SKYPE :</h3>
-							<p>seoeaze</P>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec enquiryicon">
-							<h3>Enquiry :</h3>
-							<p>info@seoeaze.com</P>
-						</div>
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec websiteicon">
-							<h3>Website :</h3>
-							<p>www.seoeaze.com</P>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12 contactinfo-innersec addressicon">
-							<h3>Address :</h3>
-							<p>10685-B Hazelhurst Dr. #19976, Houston, TX 77043</p>
-							<p>B-710, Plot No-A40, The Ithum, Sector-62, Noida (NCR)</p>
-						</div>
-					</div>
-				</div>
-				@else
-				<div class="col-lg-7 col-md-7 col-sm-12 mt-5">
-					<div class="row">
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec phoneicon">
-							<h3>Phone :</h3>
-							<p>844-SEOEAZE (7363293)</P>
-						</div>
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec skyoeicon">
-							<h3>SKYPE :</h3>
-							<p>seoeaze</P>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec enquiryicon">
-							<h3>Enquiry :</h3>
-							<p>info@seoeaze.com</P>
-						</div>
-						<div class="col-lg-6 col-md-6 col-sm-12 contactinfo-innersec websiteicon">
-							<h3>Website :</h3>
-							<p>www.seoeaze.com</P>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12 contactinfo-innersec addressicon">
-							<h3>Address :</h3>
-							<p>10685-B Hazelhurst Dr. #19976, Houston, TX 77043</P>
-						</div>
-					</div>
-				</div>
-				@endif
-			</div>
-			
-			
-	</div>
-</div>
-</section>
+		
 
 <!-- End GET IN TOUCH sec -->
 @endsection
